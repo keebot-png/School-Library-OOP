9c9d1335d4dd67378b79ecb7cfb2e46b70ef5d13
keebot-png <kee.bot@icloud.com>
2023-01-31 15:28:04 +0200

    Correct TrimmerDecorator class expression and allocated each class to their own files

--- decorator.rb
+++ decorator.rb
@@ -3,30 +3,3 @@ class Nameable
     raise NotImplementedError
   end
 end
-
-class BaseDecorator < Nameable
-  def initialize(nameable)
-    @nameable = nameable
-    super()
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < BaseDecorator
-  def correct_name
-    if @nameable.correct_name.length > 10
-      @nameable.correct_name[0..10]
-    else
-      @nameable.correct_name
-    end
-  end
-end
