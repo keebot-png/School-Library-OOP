12032e77dda9dfd2f19019496814dd0166c1ae17
keebot-png <kee.bot@icloud.com>
2023-02-01 18:25:48 +0200

    Correct student file and fix linters.

--- student.rb
+++ student.rb
@@ -3,6 +3,11 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
+  def initialize(name, age, parent_permission, classroom)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
