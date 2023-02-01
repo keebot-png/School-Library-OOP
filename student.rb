522c0bc4904c43998a938ea140615f4378f9948c
keebot-png <kee.bot@icloud.com>
2023-02-01 18:06:05 +0200

    Fix linter error

--- student.rb
+++ student.rb
@@ -3,7 +3,6 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-
   def classroom=(classroom)
     @classroom = classroom
     classroom.classroom.push(self) unless classroom.students.include?(self)
